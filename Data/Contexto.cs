fd5b8204227a6d6aaad5e1b6cc8852050a953818
Erick J. Gutierrez <50554753+erickJ27@users.noreply.github.com>
2020-03-11 16:36:39 -0300

    Add project files.

---
+++ Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcialApli2.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlite(@"Data Source=Database/Parcial2.db"));
+        }
+    }
+}
